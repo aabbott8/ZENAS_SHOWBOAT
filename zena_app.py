badfb92b2c1686649e22e45761964cec19e2b9a2
aabbott8 <167903835+aabbott8@users.noreply.github.com>
2024-04-25 13:55:08 -0600

    Update zena_app.py
    taco

--- zena_app.py
+++ zena_app.py
@@ -2,6 +2,7 @@ import streamlit
 import snowflake.connector
 
 streamlit.title('My Parents New Healthy Diner')
+"taco"
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
 my_cur = my_cnx.cursor() 
